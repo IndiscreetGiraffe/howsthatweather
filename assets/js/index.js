5f6f85ab22b1cb87999ac6ff0334f452b7091ff7
Kyleah Justice <kikij432@gmail.com>
2022-03-07 02:08:01 -0700

    I did it! I figured out how to get my stuff to appear! Going to do the rest

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 var API_Key = "bee83911dfeeb510a7e77e6f32642452";
 var searchBtn = document.querySelector("#search-btn");
-var current = document.querySelector('.current-weather');
+var current = document.querySelector('.current-header');
+var temperature = document.querySelector('.temp');
+
 
 
 searchBtn.addEventListener("click", function () {
@@ -9,8 +11,8 @@ searchBtn.addEventListener("click", function () {
 });
 
 function currentForecast() {
-    var cityName = document.getElementById("city").value;
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_Key}&units=imperial`;
+    var  currentName = document.getElementById("city").value;
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${currentName}&appid=${API_Key}&units=imperial`;
     fetch(apiURL)
     .then(function(response) {
         return response.json();
@@ -18,7 +20,8 @@ function currentForecast() {
     .then(function(data) {
         console.log(data);
 
-        current.textContent = "City: " + data.name;
+        current.textContent = "City : " + data.name;
+        temperature.textContent = "Temperature : " + data.main.temp;
 
     });
 }
