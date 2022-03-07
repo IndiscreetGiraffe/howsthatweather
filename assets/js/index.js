a23b3bcb4828ec7cb08dee1c5149d0240d105ba2
Kyleah Justice <kikij432@gmail.com>
2022-03-07 00:06:08 -0700

    got the button to click, but now it can't find the city. gonna look back at the documents and search it

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,25 @@
 var api_Key = "bee83911dfeeb510a7e77e6f32642452";
-var searchBtn = document.querySelector("#searchBtn");
+var searchBtn = document.querySelector("#search-btn");
 var currentWeather = document.querySelector(".current-weather");
 var userCity = document.querySelector("city");
 
 
 searchBtn.addEventListener("click", function () {
-    currentWeather();
+    currentForecast();
 
 });
 
-let currentWeather = function () {
+let currentForecast = function () {
     let userCity = document.getElementById("city");
     console.log(userCity);
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${api_Key}&units=imperial')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${api_Key}&units=imperial`)
     .then(function(response) {
         response.json().then(function(data) {
             console.log(data);
-            $('.current-header').text(userCity)
+            $('.current-header').text(userCity);
+            $('.temp').text('Temperature: ' + data.current.temp + ' degrees');
+
         })
     })
 }
