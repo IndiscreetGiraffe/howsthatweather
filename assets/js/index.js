6bbf5f902c7dba159734a4be2f4dd2305acbe9c3
Kyleah Justice <kikij432@gmail.com>
2022-03-06 23:45:06 -0700

    figuring out the function to call and fetch the current weather data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
-var apiKey = "bee83911dfeeb510a7e77e6f32642452";
+var api_Key = "bee83911dfeeb510a7e77e6f32642452";
 var searchBtn = document.querySelector("#searchBtn");
 var currentWeather = document.querySelector(".current-weather");
+var userCity = document.querySelector("city");
 
 
+searchBtn.addEventListener("click", function () {
+    currentWeather();
+
+});
+
+let currentWeather = function () {
+    let userCity = document.getElementById("city");
+    console.log(userCity);
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${api_Key}&units=imperial')
+    .then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            $('.current-header').text(userCity)
+        })
+    })
+}
